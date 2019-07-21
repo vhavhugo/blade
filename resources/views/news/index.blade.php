f2694c5227065807c0e197fea4e5a6047045097d
victor_hugo_valle <vhavhugo@gmail.com>
2019-07-21 20:23:56 -0300

    for e while

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -122,9 +122,21 @@
                 <li class="page-item disabled">
                   <a class="page-link" href="#" tabindex="-1">Previous</a>
                 </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                @for($i=1; $i < 10; $i++)
+                  <li class="page-item"><a class="page-link" href="#">{{$i}}</a></li>
+                @endfor
+
+                @php
+                  $i = 1;
+                @endphp
+
+                @while( $i < 10 )
+                  <li class="page-item"><a class="page-link" href="#">{{$i}}</a></li>
+
+                  @php
+                    $i++
+                  @endphp
+                @endwhile
                 <li class="page-item">
                   <a class="page-link" href="#">Next</a>
                 </li>
